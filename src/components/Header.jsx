f3dd6bba645bd0c6f2af2db4a3821990297ee6dd
Kevin <keviniannguyen@gmail.com>
2025-12-17 18:25:13 -0800

    initialize tailwind and minor changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,37 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
-import './Header.css';
+import "./Header.css";
 
-const Header = ({ }) => {
+const Header = ({}) => {
   return (
     <div className="header">
       <ul>
-        <li><Link to="/home" className='no-underline'>Home</Link></li>
-        <li><Link to="/about" className='no-underline'>About</Link></li>
-        <li><Link to="/projects" className='no-underline'>Projects</Link></li>
-        <li><Link to="/resume" className='no-underline'>Resume</Link></li>
-        <li><Link to="/contact" className='no-underline'>Contact</Link></li>
+        <li>
+          <Link to="/" className="no-underline">
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link to="/about" className="no-underline">
+            About
+          </Link>
+        </li>
+        <li>
+          <Link to="/projects" className="no-underline">
+            Projects
+          </Link>
+        </li>
+        <li>
+          <Link to="/resume" className="no-underline">
+            Resume
+          </Link>
+        </li>
+        <li>
+          <Link to="/contact" className="no-underline">
+            Contact
+          </Link>
+        </li>
       </ul>
     </div>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,38 +1,46 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { animated, useSpring } from '@react-spring/web';
-import './Home.css';
+import React from "react";
+import { Link } from "react-router-dom";
+import { animated, useSpring } from "@react-spring/web";
+import "./Home.css";
 
-import Header from '../components/Header';
+import Header from "../components/Header";
 
 const Home = () => {
-  const animation = useSpring({ opacity: 1, from: { opacity: 0 }, config: { duration: 500 } });
+  const animation = useSpring({
+    opacity: 1,
+    from: { opacity: 0 },
+    config: { duration: 500 },
+  });
 
   return (
-    <div className='body'>
-      <div className="fake-window">
+    <div className="body">
+      <div className="border-3 bg-stone-100 w-7xl h-max">
         <Header />
         <animated.div style={animation} className="animation-container">
-          <div className='home-title'>
-            <h1 className='title'>KEVIN NGUYEN</h1>
-            <h3 className='subtitle'>SOFTWARE ENGINEER</h3>
+          <div className="justify-center grid place-items-center">
+            <h1 className="text-4xl tracking-wider uppercase text-center">
+              Kevin Nguyen
+            </h1>
+            <h3 className="text-xl tracking-wide italic uppercase text-center">
+              Software Engineer
+            </h3>
           </div>
-          <div className='footer'>
+          <div className="max-w-md bottom grid grid-cols-2 gap-4 place-items-center">
             <Link
-              to="https://www.linkedin.com/in/kevin-nguyen-5b09b42b4/"
+              to="https://www.linkedin.com/in/keviniann/"
               target="_blank"
               rel="noopener noreferrer"
-              className='no-underline'
+              className="text-center"
             >
-              <p className='foot-title'>LINKEDIN</p>
+              <p className="text-xl">LINKEDIN</p>
             </Link>
             <Link
               to="https://github.com/keviniannn"
               target="_blank"
               rel="noopener noreferrer"
-              className='no-underline'
+              className="text-center"
             >
-              <p className='foot-title'>GITHUB</p>
+              <p className="text-xl">GITHUB</p>
             </Link>
           </div>
         </animated.div>
